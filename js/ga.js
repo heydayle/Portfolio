f8ca35f42419d4c87bf040ffdbae5b191d8d3f62
heyday <hungthinh.ckc@gmail.com>
2021-10-20 18:57:15 +0700

    Add GA

--- js/ga.js
+++
@@ -1,7 +0,0 @@
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-
-        gtag('config', 'G-G5V1T150XD');
-    </script>
